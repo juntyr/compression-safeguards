82cf58c8b39b93c2636cf1122a2289a4e6211058
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-12-23 20:55:51 +0000

    Split out guardrail correction

--- src/numcodecs_guardrail/__init__.py
+++ src/numcodecs_guardrail/__init__.py
@@ -1,5 +1,5 @@
-from enum import StrEnum
-from typing import Optional
+from abc import ABC, abstractmethod
+from enum import Enum
 
 import numcodecs
 import numcodecs.abc
@@ -9,42 +9,96 @@ import numcodecs.zlib
 import numpy as np
 
 
-class GuardrailErrorBound(StrEnum):
-    ABSOLUTE = "abs"
+class Guardrail(ABC):
+    kind: str
+
+    @abstractmethod
+    def check(self, data: np.ndarray, decoded: np.ndarray) -> bool:
+        pass
+
+    @abstractmethod
+    def encode_correction(self, data: np.ndarray, decoded: np.ndarray) -> bytes:
+        pass
+
+    @abstractmethod
+    def apply_correction(self, decoded: np.ndarray, correction: bytes) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def get_config(self) -> dict:
+        pass
+
+    @classmethod
+    def from_config(cls, config: dict):
+        return cls(**config)
 
 
-class Guardrail(numcodecs.abc.Codec):
-    __slots__ = ("_codec", "_lossless", "_error_bound", "_eb_abs")
+class AbsoluteErrorBoundGuardrail(Guardrail):
+    __slots__ = "_eb_abs"
+    _eb_abs: float
+
+    kind = "abs"
+
+    def __init__(self, eb_abs: float):
+        assert eb_abs is not None, "eb_abs must not be None"
+        assert eb_abs > 0.0, "eb_abs must be positive"
+
+        self._eb_abs = eb_abs
+
+    def check(self, data: np.ndarray, decoded: np.ndarray) -> bool:
+        return np.amax(np.abs(data - decoded)) <= self._eb_abs
+
+    def encode_correction(self, data: np.ndarray, decoded: np.ndarray) -> bytes:
+        error = decoded - data
+        correction = np.round(error / (self._eb_abs * 2.0)).astype(np.int64)
+
+        correction = self._lossless.encode(correction)
+
+        return numcodecs.compat.ensure_bytes(correction)
+
+    def apply_correction(self, decoded: np.ndarray, correction: bytes) -> np.ndarray:
+        correction = self._lossless.decode(correction)
+        correction = numcodecs.compat.ensure_bytes(correction)
+        correction = np.frombuffer(correction, np.int64).reshape(decoded.shape)
+        correction = correction.astype(decoded.dtype) * (self._eb_abs * 2.0)
+
+        return decoded + correction
+
+    def get_config(self):
+        return dict(eb_abs=self._eb_abs)
+
+
+class GuardrailKind(Enum):
+    ABSOLUTE = AbsoluteErrorBoundGuardrail.kind
+
+
+class GuardrailCodec(numcodecs.abc.Codec):
+    __slots__ = ("_codec", "_lossless", "_guardrail")
     _codec: numcodecs.abc.Codec
     _lossless: numcodecs.abc.Codec
-    _error_bound: GuardrailErrorBound
-    _eb_abs: Optional[float]
+    _guardrail: Guardrail
 
     codec_id = "guardrail"
 
     def __init__(
         self,
         codec: dict,
-        error_bound: str | GuardrailErrorBound,
-        eb_abs: Optional[float],
+        guardrail: str | GuardrailKind,
+        **kwargs,
     ):
         self._codec = numcodecs.registry.get_codec(codec)
         self._lossless = numcodecs.zlib.Zlib(level=9)
-        self._error_bound = (
-            error_bound
-            if isinstance(error_bound, GuardrailErrorBound)
-            else GuardrailErrorBound[error_bound]
-        )
-        self._eb_abs = eb_abs
 
-        if self._error_bound is GuardrailErrorBound.ABSOLUTE:
-            assert self._eb_abs is not None, "eb_abs must not be None"
-            assert self._eb_abs > 0.0, "eb_abs must be positive"
+        guardrail = (
+            guardrail
+            if isinstance(guardrail, GuardrailKind)
+            else GuardrailKind[guardrail]
+        )
+
+        self._guardrail = (guardrail.value)(**kwargs)
 
     def encode(self, buf):
         data = numcodecs.compat.ensure_ndarray(buf)
-        data_dtype = data.dtype
-        data_shape = data.shape
 
         assert data.dtype in (
             np.dtype("float32"),
@@ -52,58 +106,56 @@ class Guardrail(numcodecs.abc.Codec):
         ), "can only encode f32 and f64 arrays"
 
         encoded = self._codec.encode(np.copy(data))
-        decoded = self._codec.decode(np.copy(encoded))
-
         encoded = numcodecs.compat.ensure_ndarray(encoded)
-        encoded_dtype = numcodecs.compat.ensure_ndarray(encoded).dtype
-        encoded_shape = encoded.shape
 
+        assert encoded.dtype == np.dtype("uint8"), "codec must encode to bytes"
+        assert len(encoded.shape) <= 1, "codec must encode to 1D bytes"
         encoded = numcodecs.compat.ensure_bytes(encoded)
+
+        decoded = np.empty_like(data)
+        decoded = self._codec.decode(np.copy(encoded), out=decoded)
         decoded = numcodecs.compat.ensure_ndarray(decoded)
 
-        error = decoded - data
-        correction = np.round(error / (self._eb_abs * 2.0)).astype(np.int64)
+        assert decoded.dtype == data.dtype, "codec must roundtrip dtype"
+        assert decoded.shape == data.shape, "codec must roundtrip shape"
 
-        correction = self._lossless.encode(correction)
-        correction = numcodecs.compat.ensure_bytes(correction)
+        if self._guardrail.check(data, decoded):
+            # TODO: less metadata
+            correction = bytes()
+        else:
+            correction = self._guardrail.encode_correction(data, decoded)
 
-        # TODO: encode a header that details the data shape
-        return data_dtype, data_shape, encoded, encoded_dtype, encoded_shape, correction
+        # TODO: return metadata, encoded, optional correction
+        return encoded + correction
 
     def decode(self, buf, out=None):
-        # TODO: decode a header that details the data shape
-        data_dtype, data_shape, encoded, encoded_dtype, encoded_shape, correction = buf
+        assert out is not None, "can only decode into known dtype and shape"
+        out = numcodecs.compat.ensure_ndarray(out)
 
-        encoded = np.frombuffer(encoded, encoded_dtype).reshape(encoded_shape)
+        # TODO: decode the metadata into encoded and optional correction
+        encoded, correction = buf
+
         decoded = self._codec.decode(encoded)
 
-        correction = self._lossless.decode(correction)
-        correction = numcodecs.compat.ensure_bytes(correction)
-        correction = np.frombuffer(correction, np.int64).reshape(data_shape)
-        correction = correction.astype(data_dtype) * (self._eb_abs * 2.0)
-
-        corrected = decoded + correction
+        corrected = self._guardrail.apply_correction(decoded, correction)
 
         return numcodecs.compat.ndarray_copy(corrected, out)
 
     def get_config(self):
-        config = dict(
+        return dict(
             id=type(self).codec_id,
             codec=self._codec.get_config(),
-            error_bound=str(self._error_bound),
+            guardrail=self._guardrail.kind,
+            **self._guardrail.get_config(),
         )
 
-        if self._eb_abs is not None:
-            config["eb_abs"] = self._eb_abs
-
-        return config
-
     def __repr__(self):
-        params = dict(codec=self._codec, error_bound=self._error_bound)
+        config = dict(
+            codec=self._codec,
+            guardrail=self._guardrail.kind,
+            **self._guardrail.get_config(),
+        )
 
-        if self._eb_abs is not None:
-            params["eb_abs"] = self._eb_abs
-
-        repr = ", ".join(f"{p}={v!r}" for p, v in params.items())
+        repr = ", ".join(f"{p}={v!r}" for p, v in config.items())
 
         return f"{type(self).__name__}({repr})"
