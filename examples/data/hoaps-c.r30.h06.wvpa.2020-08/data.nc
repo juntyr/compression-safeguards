126021581aa99d8065c91a46b8f5eeab98292a29
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-04-16 07:35:56 +0000

    Add an example with NaNs

---
+++ examples/data/hoaps-c.r30.h06.wvpa.2020-08/data.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:316d1684d5d09f9d516e7968a02339672be6d26df9387f57624020881a0b412f
+size 39032295
---
+++ examples/data/hoaps-c.r30.h06.wvpa.2020-08/fetch.py
@@ -0,0 +1,14 @@
+import cdsapi
+
+dataset = "satellite-total-column-water-vapour-ocean"
+request = {
+    "origin": "c3s",
+    "climate_data_record_type": "icdr",
+    "temporal_aggregation": "6_hourly",
+    "year": ["2020"],
+    "month": ["08"],
+    "variable": "all"
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
