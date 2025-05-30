2e35e293a49c0abc892b83e5fd62b0f418ff7ca8
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-05-30 08:19:23 +0000

    Switch to extracted numcodecs-huffman codec

--- packages/numcodecs-safeguards/src/numcodecs_safeguards/lossless.py
+++ packages/numcodecs-safeguards/src/numcodecs_safeguards/lossless.py
@@ -5,88 +5,15 @@ Helper classes for lossless encoding for the codec with safeguards.
 __all__ = ["Lossless"]
 
 from dataclasses import dataclass, field
-from io import BytesIO
 
 import numcodecs.compat
 import numcodecs.registry
 import numpy as np
-import varint
 from compression_safeguards.cast import as_bits
-from dahuffman import HuffmanCodec as DaHuffmanCodec
-from dahuffman.huffmancodec import _EOF
 from numcodecs.abc import Codec
 from numcodecs_combinators.stack import CodecStack
+from numcodecs_huffman import HuffmanCodec
 from typing_extensions import Buffer  # MSPV 3.12
-
-
-class HuffmanCodec(Codec):
-    __slots__ = ()
-
-    codec_id: str = "safeguards.lossless.huffman"  # type: ignore
-
-    def encode(self, buf: Buffer) -> bytes:
-        a = numcodecs.compat.ensure_ndarray(buf)
-        dtype, shape = a.dtype, a.shape
-        a = as_bits(a.flatten())
-
-        huffman = DaHuffmanCodec.from_data(a)
-        encoded = huffman.encode(a)
-
-        # message: dtype shape table encoded
-        message = []
-
-        message.append(varint.encode(len(dtype.str)))
-        message.append(dtype.str.encode("ascii"))
-
-        message.append(varint.encode(len(shape)))
-        for s in shape:
-            message.append(varint.encode(s))
-
-        table = huffman.get_code_table()
-        table_no_eof = [
-            (k, e) for k, e in huffman.get_code_table().items() if k != _EOF
-        ]
-        message.append(varint.encode(len(table_no_eof)))
-        # FIXME: what about endianness
-        message.append(np.array([k for k, _ in table_no_eof]).tobytes())
-        for k, (bitsize, value) in table_no_eof:
-            message.append(varint.encode(bitsize))
-            message.append(varint.encode(value))
-        bitsize, value = table[_EOF]
-        message.append(varint.encode(bitsize))
-        message.append(varint.encode(value))
-
-        message.append(encoded)
-
-        return b"".join(message)
-
-    def decode(self, buf: Buffer, out: None | Buffer = None) -> Buffer:
-        b = numcodecs.compat.ensure_bytes(buf)
-
-        b_io = BytesIO(b)
-
-        dtype = np.dtype(b_io.read(varint.decode_stream(b_io)).decode("ascii"))
-
-        shape = tuple(
-            varint.decode_stream(b_io) for _ in range(varint.decode_stream(b_io))
-        )
-
-        table_len = varint.decode_stream(b_io)
-        table_keys = np.frombuffer(
-            b_io.read(table_len * dtype.itemsize), dtype=dtype, count=table_len
-        )
-        table = dict()
-        for k in table_keys:
-            table[k] = (varint.decode_stream(b_io), varint.decode_stream(b_io))
-        table[_EOF] = (varint.decode_stream(b_io), varint.decode_stream(b_io))
-        huffman = DaHuffmanCodec(table)
-
-        decoded = np.array(huffman.decode(b_io.read())).reshape(shape)
-
-        return numcodecs.compat.ndarray_copy(decoded, out)  # type: ignore
-
-
-numcodecs.registry.register_codec(HuffmanCodec)
 
 
 class DeltaHuffmanCodec(HuffmanCodec):
