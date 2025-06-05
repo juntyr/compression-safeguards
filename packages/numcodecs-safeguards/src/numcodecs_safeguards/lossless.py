9d6d5f0e85777ef292e7d9e91b1bc76872faef26
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-06-05 09:04:10 +0000

    Extract the BinaryDeltaCodec

--- packages/numcodecs-safeguards/src/numcodecs_safeguards/lossless.py
+++ packages/numcodecs-safeguards/src/numcodecs_safeguards/lossless.py
@@ -6,45 +6,12 @@ __all__ = ["Lossless"]
 
 from dataclasses import dataclass, field
 
-import numcodecs.compat
-import numcodecs.registry
-import numpy as np
-from compression_safeguards.utils.cast import as_bits
+import numcodecs
 from numcodecs.abc import Codec
 from numcodecs_combinators.best import PickBestCodec
 from numcodecs_combinators.stack import CodecStack
+from numcodecs_delta import BinaryDeltaCodec
 from numcodecs_huffman import HuffmanCodec
-from typing_extensions import Buffer  # MSPV 3.12
-
-
-class BinaryDeltaCodec(Codec):
-    __slots__ = ()
-
-    codec_id: str = "safeguards.lossless.binary_delta"  # type: ignore
-
-    def encode(self, buf: Buffer) -> Buffer:
-        a = numcodecs.compat.ensure_ndarray(buf)
-        a_bits = as_bits(a.flatten())
-
-        a_bits_delta = a_bits.copy()
-        a_bits_delta[1:] = np.diff(a_bits)
-
-        a_delta = a_bits_delta.view(a.dtype).reshape(a.shape)
-
-        return a_delta
-
-    def decode(self, buf: Buffer, out: None | Buffer = None) -> Buffer:
-        a_delta = numcodecs.compat.ensure_ndarray(buf)
-        a_bits_delta = as_bits(a_delta.flatten())
-
-        a_bits = np.cumsum(a_bits_delta, dtype=a_bits_delta.dtype)
-
-        a = a_bits.view(a_delta.dtype).reshape(a_delta.shape)
-
-        return numcodecs.compat.ndarray_copy(a, out)  # type: ignore
-
-
-numcodecs.registry.register_codec(BinaryDeltaCodec)
 
 
 def _default_lossless_for_safeguards():
