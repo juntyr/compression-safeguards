Fix overflow

src/compression_safeguards/safeguards/pointwise/same.py
@@ -164,8 +164,9 @@ class SameValueSafeguard(PointwiseSafeguard):
 
         Lower(valuef) <= valid_below[dataf_bits == valuef_bits] <= Upper(valuef)
 
-        below_upper = np.array(from_total_order(valuef_total - 1, data.dtype))
-        above_lower = np.array(from_total_order(valuef_total + 1, data.dtype))
+        with np.errstate(over="ignore", under="ignore"):
+            below_upper = np.array(from_total_order(valuef_total - 1, data.dtype))
+            above_lower = np.array(from_total_order(valuef_total + 1, data.dtype))
 
         # non-value elements must exclude value from their interval,
         #  leading to a union of two intervals, below and above value

src/compression_safeguards/safeguards/pointwise/sign.py
@@ -138,8 +138,9 @@ class SignPreservingSafeguard(PointwiseSafeguard):
         assert np.all(~np.isnan(offsetf)), "offset must not contain NaNs"
         offsetf_total: np.ndarray = to_total_order(offsetf)
 
-        below_upper = np.array(from_total_order(offsetf_total - 1, data.dtype))
-        above_lower = np.array(from_total_order(offsetf_total + 1, data.dtype))
+        with np.errstate(over="ignore", under="ignore"):
+            below_upper = np.array(from_total_order(offsetf_total - 1, data.dtype))
+            above_lower = np.array(from_total_order(offsetf_total + 1, data.dtype))
 
         dataf = data.flatten()
         valid = (

src/compression_safeguards/utils/cast.py
@@ -190,7 +190,11 @@ def to_total_order(a: np.ndarray[S, np.dtype[T]]) -> np.ndarray[S, np.dtype[U]]:
 
     if np.issubdtype(a.dtype, np.signedinteger):
         shift = np.iinfo(a.dtype).max  # type: ignore
-        return a.view(utype) + np.array(shift, dtype=utype) + 1
+        with np.errstate(
+            over="ignore",
+            under="ignore",
+        ):
+            return a.view(utype) + np.array(shift, dtype=utype) + 1
 
     if not np.issubdtype(a.dtype, np.floating):
         raise TypeError(f"unsupported interval type {a.dtype}")
@@ -233,7 +237,11 @@ def from_total_order(
 
     if np.issubdtype(dtype, np.signedinteger):
         shift = np.iinfo(dtype).max  # type: ignore
-        return a.view(dtype) + shift + 1
+        with np.errstate(
+            over="ignore",
+            under="ignore",
+        ):
+            return a.view(dtype) + shift + 1
 
     if not np.issubdtype(dtype, np.floating):
         raise TypeError(f"unsupported interval type {dtype}")