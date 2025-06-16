Fix equal_nan for sign and handle -0.0 / +0.0 correctly

src/compression_safeguards/safeguards/pointwise/sign.py
@@ -139,13 +139,32 @@ class SignPreservingSafeguard(PointwiseSafeguard):
         offsetf_total: np.ndarray = to_total_order(offsetf)
 
         with np.errstate(over="ignore", under="ignore"):
-            below_upper = np.array(from_total_order(offsetf_total - 1, data.dtype))
-            above_lower = np.array(from_total_order(offsetf_total + 1, data.dtype))
+            if np.issubdtype(data.dtype, np.floating):
+                # special case for floating point -0.0 / +0.0 which both have
+                #  zero sign and thus have weird below / above intervals
+                info = np.finfo(data.dtype)  # type: ignore
+                below_upper = np.array(
+                    np.where(
+                        offsetf == 0,
+                        -info.smallest_subnormal,
+                        from_total_order(offsetf_total - 1, data.dtype),
+                    )
+                )
+                above_lower = np.array(
+                    np.where(
+                        offsetf == 0,
+                        info.smallest_subnormal,
+                        from_total_order(offsetf_total + 1, data.dtype),
+                    )
+                )
+            else:
+                below_upper = np.array(from_total_order(offsetf_total - 1, data.dtype))
+                above_lower = np.array(from_total_order(offsetf_total + 1, data.dtype))
 
         dataf = data.flatten()
         valid = (
             Interval.empty_like(dataf)
-            .preserve_signed_nan(dataf, equal_nan=False)
+            .preserve_signed_nan(dataf, equal_nan=True)
             .preserve_non_nan(dataf)
         )
 

tests/test_intervals.py
@@ -53,13 +53,15 @@ def test_sign():
                 [
                     [
                         _minimum(np.dtype(float)),
-                        _minimum(np.dtype(float)),
-                        _minimum(np.dtype(float)),
+                        -np.inf,
+                        -np.inf,
                         -0.0,
                         0.0,
                         np.finfo(float).smallest_subnormal,
                         np.finfo(float).smallest_subnormal,
-                        np.finfo(float).smallest_subnormal,
+                        np.array(as_bits(np.array(np.inf, dtype=float)) + 1).view(
+                            float
+                        ),
                     ]
                 ]
             )
@@ -71,13 +73,15 @@ def test_sign():
             np.array(
                 [
                     [
-                        -np.finfo(float).smallest_subnormal,
+                        -np.array(as_bits(np.array(np.inf, dtype=float)) + 1).view(
+                            float
+                        ),
                         -np.finfo(float).smallest_subnormal,
                         -np.finfo(float).smallest_subnormal,
                         -0.0,
                         0.0,
-                        _maximum(np.dtype(float)),
-                        _maximum(np.dtype(float)),
+                        np.inf,
+                        np.inf,
                         _maximum(np.dtype(float)),
                     ]
                 ]