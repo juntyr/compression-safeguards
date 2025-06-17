c9014bd50da5f21b7f0e801b89dcd1566e3cfb74
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-06-17 12:28:15 +0000

    Add some compression to the precipitation example

--- examples/data/era5-lh/fetch.py
+++ examples/data/era5-lh/fetch.py
@@ -3,16 +3,13 @@ import cdsapi
 dataset = "reanalysis-era5-single-levels"
 request = {
     "product_type": ["reanalysis"],
-    "variable": [
-        "surface_latent_heat_flux",
-        "land_sea_mask"
-    ],
+    "variable": ["surface_latent_heat_flux", "land_sea_mask"],
     "year": ["2024"],
     "month": ["04"],
     "day": ["02"],
     "time": ["12:00"],
     "data_format": "netcdf",
-    "download_format": "unarchived"
+    "download_format": "unarchived",
 }
 
 client = cdsapi.Client()
