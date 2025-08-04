dc187589f25d457a9a6aebc5fd74535ef41a1e11
Juniper Tyree <juniper.tyree@helsinki.fi>
2025-08-04 06:18:43 +0300

    Update QoI cli

--- src/compression_safeguards/safeguards/_qois/__main__.py
+++ src/compression_safeguards/safeguards/_qois/__main__.py
@@ -1,33 +1,28 @@
 import numpy as np
 
-from .expr.array import Array
+from ..stencil.qoi import StencilExpr
+from . import StencilQuantityOfInterest
 from .expr.constfold import FoldedScalarConst
-from .expr.data import Data
-from .lexer import QoILexer
-from .parser import QoIParser
 
 if __name__ == "__main__":
-    lexer = QoILexer()
-    parser = QoIParser(
-        x=Data(index=(1,)),
-        X=Array(Data(index=(0,)), Data(index=(1,)), Data(index=(2,))),
-        I=(1,),
-    )
     while True:
         try:
-            text = input("qoi > ")
+            qoi = input("qoi > ")
         except EOFError:
             break
-        if len(text) == 0:
+        if len(qoi) == 0:
             break
         try:
-            expr = parser.parse(text, lexer.tokenize(text))
-            if expr is None:
+            qoi_expr = StencilQuantityOfInterest(
+                StencilExpr(qoi), stencil_shape=(3,), stencil_I=(1,)
+            )
+            if qoi_expr is None:
                 continue
-            print(f"parsed: {expr!r}")
+            print(f"parsed: {qoi_expr!r}")
             Xs = np.array([[1.0, 2.0, 3.0]])
-            expr = FoldedScalarConst.constant_fold_expr(expr, Xs.dtype)
-            print(f"folded: {expr!r}")
-            print(f"eval: {expr.eval((1,), Xs, dict())}")
+            print(
+                f"folded: {FoldedScalarConst.constant_fold_expr(qoi_expr._expr, Xs.dtype)!r}"
+            )
+            print(f"eval: {qoi_expr.eval(Xs[:, 1], Xs, dict())}")
         except Exception as err:
             print(f"{type(err).__name__}: {err}")
