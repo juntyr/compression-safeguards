Fix broadcast bug in power

src/compression_safeguards/safeguards/_qois/expr/power.py
@@ -77,25 +77,25 @@ class ScalarPower(Expr):
             # check not just for x < 0 but also for x == -0.0
             return (x < 0) | ((1 / x) < 0)  # type: ignore
 
-        # evaluate a
-        a = self._a
+        # evaluate a, b, and power(a, b)
+        a, b = self._a, self._b
         av = a.eval(X.shape, Xs, late_bound)
-
-        # rewrite a ** b as e^(b*ln(fake_abs(a)))
-        # this is mathematically incorrect for a <= 0 but works for deriving
-        #  error bounds since fake_abs handles the error bound flips
-        Xs_lower, Xs_upper = ScalarExp(
-            Exponential.exp,
-            ScalarMultiply(self._b, ScalarLog(Logarithm.ln, ScalarFakeAbs(self._a))),
-        ).compute_data_bounds(expr_lower, expr_upper, X, Xs, late_bound)
+        bv = b.eval(X.shape, Xs, late_bound)
+        exprv = np.power(av, bv)
 
         # powers of negative numbers are just too tricky since
         #  they easily become NaN, so let's force the exact same
         #  data for them
-        Xs_lower = np.where(_is_negative(av), Xs, Xs_lower)  # type: ignore
-        Xs_upper = np.where(_is_negative(av), Xs, Xs_upper)  # type: ignore
+        expr_lower = np.where(_is_negative(av), exprv, expr_lower)  # type: ignore
+        expr_upper = np.where(_is_negative(av), exprv, expr_upper)  # type: ignore
 
-        return Xs_lower, Xs_upper
+        # rewrite a ** b as e^(b*ln(fake_abs(a)))
+        # this is mathematically incorrect for a <= 0 but works for deriving
+        #  error bounds since fake_abs handles the error bound flips
+        return ScalarExp(
+            Exponential.exp,
+            ScalarMultiply(self._b, ScalarLog(Logarithm.ln, ScalarFakeAbs(self._a))),
+        ).compute_data_bounds(expr_lower, expr_upper, X, Xs, late_bound)
 
     def __repr__(self) -> str:
         return f"{self._a!r} ** {self._b!r}"

tests/test_qoi_eb_pw.py
@@ -514,9 +514,11 @@ def test_late_bound_constant():
         f=np.array([16, 8, 4, 2, 1, 0]).reshape(2, 3),
     )
 
+    imax = np.iinfo(data.dtype).max
+
     valid = safeguard.compute_safe_intervals(data, late_bound=late_bound)
-    assert np.all(valid._lower == (data.flatten() - np.array([16, 8, 4, 2, 1, 0])))
-    assert np.all(valid._upper == (data.flatten() + np.array([16, 8, 4, 2, 1, 0])))
+    assert np.all(valid._lower == np.array([0 - 16, 1 - 8, 2 - 4, 3 - 2, 4 - 1, 1]))
+    assert np.all(valid._upper == np.array([0 + 16, 1 + 8, 2 + 4, 3 + 2, 4 + 1, imax]))
 
     ok = safeguard.check_pointwise(data, -data, late_bound=late_bound)
     assert np.all(ok == np.array([True, True, True, False, False, False]).reshape(2, 3))

tests/test_qoi_eb_stencil.py
@@ -1220,8 +1220,7 @@ def test_late_bound_constant():
         zero=0,
     )
 
-    imin = np.iinfo(data.dtype).min
-    imax = np.iinfo(data.dtype).max
+    imin, imax = np.iinfo(data.dtype).min, np.iinfo(data.dtype).max
 
     valid = safeguard.compute_safe_intervals(data, late_bound=late_bound)
     assert np.all(valid._lower == np.array([imin, imin, imin, 3 - 16, 4 - 8, 5 - 4]))
@@ -1249,9 +1248,6 @@ def test_late_bound_constant():
         f=np.array([[16, 8, 4], [16, 8, 4]]),
         zero=0,
     )
-
-    imin = np.iinfo(data.dtype).min
-    imax = np.iinfo(data.dtype).max
 
     valid = safeguard.compute_safe_intervals(data, late_bound=late_bound)
     assert np.all(valid._lower == (data.flatten() - np.array([16, 8, 4, 16, 8, 4])))