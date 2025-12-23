b205ee3d11ffce81c5e8bbe2bf343eca6f7b1042
Juniper Tyree <juniper.tyree@helsinki.fi>
2025-12-23 12:22:18 +0200

    fix isabel download script

--- examples/data/isabel/fetch.py
+++ examples/data/isabel/fetch.py
@@ -7,10 +7,10 @@ base_url = "https://cloud.sdsc.edu/v1/AUTH_sciviscontest/2004/isabeldata/"
 
 with urlopen(base_url + "Pf48.bin.gz") as response_gz:
     with gzip.open(response_gz, "rb") as response:
-        with open("Pf48.bin2", "wb") as file:
+        with open("Pf48.bin", "wb") as file:
             shutil.copyfileobj(response, file)
 
 with urlopen(base_url + "Uf48.bin.gz") as response_gz:
     with gzip.open(response_gz, "rb") as response:
-        with open("Uf48.bin2", "wb") as file:
+        with open("Uf48.bin", "wb") as file:
             shutil.copyfileobj(response, file)
