cac45c6dc50df43f357cf3c369ab50129d2d3140
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-01-26 16:33:50 +0200

    Fix formatting

--- docs/hooks/edit_uri.py
+++ docs/hooks/edit_uri.py
@@ -2,6 +2,7 @@
 
 # Allows overriding the edit_url from the yaml frontmatter
 
+
 def on_page_context(context, page, config, **kwargs):
     if "edit_uri" in page.meta:
         page.edit_url = config["repo_url"] + config["edit_uri"] + page.meta["edit_uri"]
